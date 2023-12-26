47db5ea53e8dd4c9b290aee62fcf8bc28dee82bc
Oleksandr Sikorskyi <o.sikorskyi.vn@gmail.com>
2023-12-26 22:28:25 +0200

    added help page

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
-
-async function insertBlock(file) {
-    let insert_to = document.getElementById("inserted");
-    let myObject = await fetch(file);
-    let myText = await myObject.text();
-    insert_to.innerHTML = myText;
-    let css_link = document.getElementById("included_css");
-    css_link.href = "./html/lab/lab_1/sd.css";
+const destinationHTML = document.getElementById("inserted");
+const extraCssLink = document.getElementById("extra_css");
+async function insertBlock(sourceHTML, sourceCSS="") {
+    let myObject = await fetch(sourceHTML);
+    if(myObject.status==200){
+      let myText = await myObject.text();
+      destinationHTML.innerHTML = myText;
+      extraCssLink.href = sourceCSS;
+    }
+    else{
+      destinationHTML.innerHTML = `<p>${myObject.statusText}</p>`;
+    }
+    
   }
-insertBlock("./html/lab/lab_1/subject_description.html")
+insertBlock("./html/lab/lab_1/sd.html", "./html/lab/lab_1/sd.css")
+const help_button = document.getElementById("help_button");
+help_button.addEventListener("click", helpHandler);
+async function helpHandler(){
+  let HTML_file = "./html/help/help.html";
+  let CSS_file = "./html/help/help.css";
+  insertBlock(HTML_file, CSS_file);
+};
