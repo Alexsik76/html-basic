09695fa29bc9f385fc883b50dcb396b7a2bcf00e
Oleksandr Sikorskyi <o.sikorskyi.vn@gmail.com>
2023-12-28 14:26:32 +0200

    fxed css

--- main.js
+++ main.js
@@ -14,9 +14,16 @@ async function insertBlock(sourceHTML, sourceCSS="") {
   }
 insertBlock("./html/lab/lab_1/sd.html", "./html/lab/lab_1/sd.css")
 const help_button = document.getElementById("help_button");
+const sd_button = document.getElementById("sd_button");
 help_button.addEventListener("click", helpHandler);
+sd_button.addEventListener("click", sdHandler);
 async function helpHandler(){
   let HTML_file = "./html/help/help.html";
   let CSS_file = "./html/help/help.css";
+  await insertBlock(HTML_file, CSS_file);
+};
+async function sdHandler(){
+  let HTML_file = "./html/lab/lab_1/sd.html";
+  let CSS_file = "./html/lab/lab_1/sd.css";
   await insertBlock(HTML_file, CSS_file);
 };
